e98927246a4b0c5d65a3d86bda34add1219eb7e3
Derolaa <31681901+Derolaa@users.noreply.github.com>
2017-11-01 11:18:18 -0400

    Update BlackJackController.java
    Not sure about this so commented out what I have.

--- FlamingoJavaFX/src/main/java/app/controller/BlackJackController.java
+++ FlamingoJavaFX/src/main/java/app/controller/BlackJackController.java
@@ -1,6 +1,9 @@
 package app.controller;
 
 import java.net.URL;
+
+import java.util.Iterator;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import app.Flamingo;
@@ -8,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import pkgCore.GamePlay;
 import pkgCore.Table;
+import pkgEnum.eAction;
 
 public class BlackJackController implements Initializable {
 	private Flamingo FlamingoGame;
@@ -23,13 +27,41 @@ public class BlackJackController implements Initializable {
 
 	public void btnSitLeave_Click(ActionEvent event)
 	{
-		//TODO: Implement this.  Create a new 'Action', send the 'Sit' or 'Leave' action to the Hub.
+		
+		Action act = new Action(eAction.Sit, FlamingoGame.getPlayer());
+		
+		FlamingoGame.messageSend(act);
 		
 	}
 	public void HandleTableState(Table t) {
 
-		//TODO: Implement this.
-	}
+		/*
+		 * Iterator x = t.getHmPlayer().entrySet().itterator();
+		 *
+		 lblPlayerPos1.setText("");
+		lblPlayerPos2.setText("");
+		btnPos1SitLeave.setText("Sit");
+		btnPos2SitLeave.setText("Sit");
+		
+		if (t.getHmPlayer().size() > 0) {
+			btnPos1SitLeave.setVisible(false);
+			btnPos2SitLeave.setVisible(false);
+		}
+		
+		while (x.hasNext()) {
+			Map.Entry ent = (Map.Entry) x.next();
+			Player player = (Player)ent.getValue();
+			
+			if (player.getiPlayerPosition() == 1) {
+				iblPlayerPos1.setText(player.getPlayerName());
+				btnPos1SitLeave.setText("Leave Game");
+			}
+			else if (player.getiPlayerPosition() == 2) {
+				lblPlayerPos2.setText(player.getPlayerName());
+				btnPos2SitLeaave.setText("Leave Game");
+			}
+		}
+	}*/
 
 	public void HandleGameState(GamePlay gp) {
 
